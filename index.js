9de667e60424e64d9c2065ceccc38c1c1be79fa3
unknown <gujwarpooja@gmail.com>
2024-11-18 11:48:56 +0530

    chore:Add some missing querys

--- index.js
+++ index.js
@@ -56,15 +56,15 @@ app.delete("/books/:id", async (req, res) => {
 });
 
 app.put("/books/:id",async(req,res)=>{
-  const bookId = req.params.id
-  const updateBook = req.body
+  const bookId = req.params.id;
+  const updateBook = req.body;
   try{
-    const updatedbook = Books.findByIdAndUpdate(bookId,updateBook,{new:true})
+    const updatedbook =await Books.findByIdAndUpdate(bookId,updateBook,{new:true})
     if(!updateBook){
-      res.status(404).json({message:"Book not found"})
+      return res.status(404).json({message:"Book not found"})
     }
     
-  res.status(200).json(updatedBook)
+  res.status(200).json(updatedbook)
 
   }catch(error){
     res.status(500).json({error:"Internal server error"})
